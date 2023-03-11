3a5696a2a379894f169f1dc543bda0d2d0508ac9
Amar1430 <amar1430@outlook.com>
2023-03-11 21:03:17 +0300

    "

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 const loginUser = document.querySelector("#loginUser");
 const profillUser = document.querySelector("#profillUser");
 const user = document.querySelector("#user").innerText;
+console.log(user)
 if (user != "") {
   loginUser.classList.add("dn");
   profillUser.classList.remove("dn");
