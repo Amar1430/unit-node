b92fdc94a0aa48ceaf417bd927dbfccc3e74c0ed
Amar1430 <amar1430@outlook.com>
2023-04-14 15:11:35 +0300

    fix list open and add to list profile

--- public/main.js
+++ public/main.js
@@ -1,6 +1,22 @@
 const loginUser = document.querySelector("#loginUser");
 const profillUser = document.querySelector("#profillUser");
+const loginUserA = document.querySelector("#loginUserA");
+const profillUserA = document.querySelector("#profillUserA");
 const user = document.querySelector("#user").innerText;
+const menu = document.querySelector("#menu")
+const headerA = document.querySelectorAll(".headerA")
+
+
+
+
+
+menu.addEventListener("click", (e) => {
+  menu.classList.toggle("menuActive")
+  headerA.forEach(item => {
+    item.classList.toggle("db")
+  });
+})
+
 
 if (user != "") {
   loginUser.classList.add("dn");
@@ -8,4 +24,12 @@ if (user != "") {
 } else {
   loginUser.classList.remove("dn");
   profillUser.classList.add("dn");
+}
+
+if (user != "") {
+  loginUserA.classList.add("dn");
+  profillUserA.classList.remove("dn");
+} else {
+  loginUserA.classList.remove("dn");
+  profillUserA.classList.add("dn");
 }
